3814af739e5d98ab34ddf83321be3bbc89524f3d
root <root@curie.nt.ntnu.no>
2012-03-15 12:05:09 +0100

    Removed password from settings.py, creating new pass

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # Django settings for timeliste project.
 
+import rtconfig
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -14,7 +15,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'timeliste',                      # Or path to database file if using sqlite3.
         'USER': 'timeliste_admin',                      # Not used with sqlite3.
-        'PASSWORD': '5up3rM4nn',                  # Not used with sqlite3.
+        'PASSWORD': rtconfig.DBPASS,                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
