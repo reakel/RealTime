bc8e0f746076b4e28c7c78fa22eeab2dbd9d06df
root <root@curie.nt.ntnu.no>
2012-03-16 18:17:09 +0100

    Renamed to realtime

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for timeliste project.
+# Django settings for realtime project.
 
 import rtconfig
 DEBUG = True
@@ -13,8 +13,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'timeliste',                      # Or path to database file if using sqlite3.
-        'USER': 'timeliste_admin',                      # Not used with sqlite3.
+        'NAME': 'realtime',                      # Or path to database file if using sqlite3.
+        'USER': 'realtime_admin',                      # Not used with sqlite3.
         'PASSWORD': rtconfig.DBPASS,                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -57,16 +57,16 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static/timeliste'
+STATIC_ROOT = '/var/www/static/realtime/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/timeliste/static/'
+STATIC_URL = '/static/realtime/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/timeliste/static/admin/'
+ADMIN_MEDIA_PREFIX = '/static/realtime/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -101,13 +101,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'timeliste.urls'
+ROOT_URLCONF = 'realtime.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/usr/local/wsgi/timeliste/templates',
+    '/usr/local/wsgi/realtime/templates',
 )
 
 INSTALLED_APPS = (
@@ -147,5 +147,5 @@ LOGGING = {
         },
     }
 }
-LOGIN_URL = '/timeliste/login'
+LOGIN_URL = '/realtime/login'
 AUTH_PROFILE_MODULE = 'timesheet.UserProfile'
